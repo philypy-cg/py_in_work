987839c79e6626fdcf5482dca936b80fdb0fbcc5
Philypy Gomes <phcg.dev@gmail.com>
2023-11-04 00:34:25 -0300

    varandconst

---
+++ variaveis_constantes.py
@@ -0,0 +1,13 @@
+nome = 'Philypy'
+idade = 30
+
+nome, idade = 'Jeh', 30
+
+
+print(nome, idade)
+
+limite_diario_saque = 1000
+
+BRAZILIAN_STATES = ['PE', 'SC', 'PR', 'RS', 'ES']
+
+print(BRAZILIAN_STATES)
